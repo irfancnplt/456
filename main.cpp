bb16930fdf8a90347ef542f03dee4c0d4a0bcaff
İRFAN CANPOLAT <114351136+irfancnplt@users.noreply.github.com>
2022-10-09 13:39:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,7 @@
-#include <iostream>
+import json
 
-using namespace std;
-void bolum(int dboyutu, int dizi[],int bol){
-	int sayac=0;
-	while(sayac != dboyutu)
-	{
-		if(sayac % bol !=0)
-		{
-			cout << dizi[sayac];
-		}
-		else if(sayac % bol == 0)
-		{
-			cout << endl;
-			cout<< dizi[sayac];
-		}
-		sayac++;
-	}
-}
-int main(int argc, char** argv) {
-	int dizi[9]={1,2,3,4,5,6,7,8,9};
-	int dboyutu=9;
-	int bol;
-	cout << "bolunmesi gereken sayi:";
-	cin>>bol;
-	if(dboyutu>bol && bol>0)
-	{
-		bolum(9,dizi,bol);
-	}
-	else 
-	{
-		cout << "Hatali Islem...";
-	}
-	return 0;
-}
+with open('C:\\Users\\LENOVO\\Desktop\\data\\Data.json') as f:
+  veri = json.load(f)
+
+print(veri)
+
